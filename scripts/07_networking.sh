c979dfaffa85e4d36a07fdfbc955faba19c5588f
Charles Sanquer <csanquer@kilix.fr>
2016-07-28 11:33:50 +0200

    fix networking

--- scripts/07_networking.sh
+++ scripts/07_networking.sh
@@ -32,7 +32,7 @@ echo "$resolvConf" | sudo tee $resolvConfFile
 #service networking restart
 
 rm -f /etc/udev/rules.d/70-persistent-net.rules;
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+touch /etc/udev/rules.d/70-persistent-net.rules;
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
 rm -rf /dev/.udev/ /var/lib/dhcp/*;
 
